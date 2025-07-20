58d822c363ca57d1925dc8381ec78653782a83a4
Abraham Galue <abrahamgalue512@gmail.com>
2025-07-20 18:57:37 -0400

    Automatically added products using faker

--- ts-2/src/app/main.ts
+++ ts-2/src/app/main.ts
@@ -1,15 +1,27 @@
-import { addProduct } from './products/product.service';
+import { addProduct, products } from './products/product.service';
+import { faker } from '@faker-js/faker';
 
-addProduct({
-  id: '1',
-  title: 'p1',
-  createdAt: new Date(),
-  updatedAt: new Date(),
-  stock: 90,
-  category: {
-    id: '789',
-    name: 'car',
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  },
-});
+for (let index = 0; index < 50; index++) {
+  addProduct({
+    id: faker.string.uuid(),
+    description: faker.commerce.productDescription(),
+    image: faker.image.url(),
+    color: faker.color.rgb(),
+    size: faker.helpers.arrayElement(['M', 'S', 'XL', 'L']),
+    price: parseInt(faker.commerce.price()),
+    title: faker.commerce.productName(),
+    createdAt: faker.date.recent(),
+    updatedAt: faker.date.recent(),
+    stock: faker.number.int({ min: 10, max: 100 }),
+    category: {
+      id: '789',
+      name: faker.commerce.department(),
+      createdAt: faker.date.recent(),
+      updatedAt: faker.date.recent(),
+    },
+    isNew: faker.datatype.boolean(),
+    tags: faker.helpers.arrayElements(['tag1', 'tag2', 'tag3', 'tag4']),
+  });
+}
+
+console.log(products);
