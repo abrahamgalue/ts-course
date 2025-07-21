3ba85ec4feabf2e0509e3a35addd273922b53acf
Abraham Galue <abrahamgalue512@gmail.com>
2025-07-21 08:19:10 -0400

    Utility types Omit and Pick

--- ts-2/src/app/main.ts
+++ ts-2/src/app/main.ts
@@ -3,24 +3,16 @@ import { faker } from '@faker-js/faker';
 
 for (let index = 0; index < 50; index++) {
   addProduct({
-    id: faker.string.uuid(),
     description: faker.commerce.productDescription(),
     image: faker.image.url(),
     color: faker.color.rgb(),
     size: faker.helpers.arrayElement(['M', 'S', 'XL', 'L']),
     price: parseInt(faker.commerce.price()),
     title: faker.commerce.productName(),
-    createdAt: faker.date.recent(),
-    updatedAt: faker.date.recent(),
     stock: faker.number.int({ min: 10, max: 100 }),
-    category: {
-      id: '789',
-      name: faker.commerce.department(),
-      createdAt: faker.date.recent(),
-      updatedAt: faker.date.recent(),
-    },
     isNew: faker.datatype.boolean(),
     tags: faker.helpers.arrayElements(['tag1', 'tag2', 'tag3', 'tag4']),
+    categoryId: faker.string.uuid(),
   });
 }
 
