915199c1122a12a9759f5d16163d3f1ac56c0664
Abraham Galue <abrahamgalue512@gmail.com>
2025-07-21 08:38:57 -0400

    Access to the typing by the index

--- ts-2/src/app/products/product.service.ts
+++ ts-2/src/app/products/product.service.ts
@@ -26,7 +26,7 @@ export const addProduct = (data: CreateProductDto): Product => {
   return newProduct;
 };
 
-export const updateProduct = (id: string, changes: UpdateProductDto) => {
+export const updateProduct = (id: Product['id'], changes: UpdateProductDto) => {
   const index = products.findIndex((item) => item.id === id);
   const prevData = products[index];
   products[index] = {
