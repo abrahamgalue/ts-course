a8100c1f7e036c7b30dc194277f2a877709d5e93
Abraham Galue <abrahamgalue512@gmail.com>
2025-07-24 10:54:22 -0400

    Interfaces to be implemented by classes

---
+++ ts-poo/src/11-interfaces.ts
@@ -0,0 +1,50 @@
+interface Driver {
+  database: string;
+  password: string;
+  port: number;
+  connect(): void;
+  disconnect(): void;
+  isConnected(name: string): boolean;
+}
+
+// const driver: Driver = {
+//   database: '',
+//   password: '',
+//   port: 23,
+// };
+
+class PostgresDriver implements Driver {
+  constructor(
+    public database: string,
+    public password: string,
+    public port: number
+  ) {}
+
+  connect(): void {
+    // code
+  }
+  disconnect(): void {
+    // code
+  }
+  isConnected(name: string): boolean {
+    return true;
+  }
+}
+
+class OracleDriver implements Driver {
+  constructor(
+    public database: string,
+    public password: string,
+    public port: number
+  ) {}
+
+  connect(): void {
+    // code
+  }
+  disconnect(): void {
+    // code
+  }
+  isConnected(name: string): boolean {
+    return false;
+  }
+}
